65d62578fbf6e3df278725b6cd1f749670536828
bomikang <bomiisworldstar@gmail.com>
2017-01-03 16:49:37 +0900

    importdata수정

--- src/main/java/kr/or/dgit/bigdata/post_program/service/ImportDataFiles.java
+++ src/main/java/kr/or/dgit/bigdata/post_program/service/ImportDataFiles.java
@@ -4,4 +4,15 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ImportDataFiles {
+	private static ImportDataFiles instance = new ImportDataFiles();
+	
+	public static ImportDataFiles getInstance(){
+		return instance;
+	}
+	
+	private ImportDataFiles(){}
+	
+	public void getDataFiles(){
+		String sql = "";
+	}
 }
